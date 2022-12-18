bdb3620c109052c45297a4944fe8092f3e582abf
Paulo Henrique <jwcfree11@gmail.com>
2022-12-18 12:24:05 -0300

    Add Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <>
       <Header />
-      <Post author="Paulo Henrique" content="Um conteúdo muito legal" />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post author="Paulo Henrique" content="Um conteúdo muito legal" />
+          <Post author="Paulo Henrique" content="Um conteúdo muito legal" />
+        </main>
+      </div>
     </>
   );
 }
