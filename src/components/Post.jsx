04a8f12a4e3751521638e87a09f55d639aeed9c3
Paulo Henrique <jwcfree11@gmail.com>
2022-12-27 10:39:42 -0300

    Add Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -39,6 +39,13 @@ export function Post(props) {
           <a href="#">#rocketseat</a>
         </p>
       </div>
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário"/>
+        <footer>
+        <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
