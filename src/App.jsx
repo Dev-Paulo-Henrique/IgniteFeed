02e303fe9a9101852800608133a42e8f240f8a9f
Paulo Henrique <jwcfree11@gmail.com>
2023-01-06 13:29:01 -0300

    Add Fake Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,60 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
-import { Sidebar } from './components/Sidebar'
+import { Sidebar } from "./components/Sidebar";
 import "./global.css";
 import styles from "./App.module.css";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/67799522",
+      name: "Paulo Henrique",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+      { type: "hashtag", content: "#novoprojeto"},
+    ],
+    publishedAt: new Date("2023-01-06 12:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/6643122",
+      name: "Mayk Brito",
+      role: "Mobile Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+      { type: "hashtag", content: "#rocketseat"},
+    ],
+    publishedAt: new Date("2022-05-01 19:05:00"),
+  },
+];
 
 export function App() {
   return (
     <>
       <Header />
       <div className={styles.wrapper}>
-        <Sidebar/>
+        <Sidebar />
         <main>
-          <Post author="Paulo Henrique" content="Um conteúdo muito legal" />
-          <Post author="Paulo Henrique" content="Um conteúdo muito legal" />
-          <Post author="Paulo Henrique" content="Um conteúdo muito legal" />
-          <Post author="Paulo Henrique" content="Um conteúdo muito legal" />
-          <Post author="Paulo Henrique" content="Um conteúdo muito legal" />
+          {posts.map((post) => {
+            return <Post author={post.author} content={post.content} publishedAt={post.publishedAt}/>;
+          })}
         </main>
       </div>
     </>
